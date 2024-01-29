8fa5a198999184e286823a933e0e79b0a555eae5
sadyplabon <plabon.pen@gmail.com>
2024-01-29 20:32:33 +0600

    create register page and connect to database

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,39 @@
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
 import HomeMenu from "./components/layout/HomeMenu";
+import SectionHeaders from "./components/layout/SectionHeaders";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"our story"} mainHeader={"about us"} />
+        <p className="max-w-2xl mx-auto mt-4 text-gray-500">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </section>
+      <section className="text-center">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"contact us"}
+        />
+        <a
+          href="tel:+8801717090609"
+          className="text-4xl underline text-gray-500 font-semibold"
+        >
+          +880 1717 090609
+        </a>
+      </section>
     </>
   );
 }
---
+++ src/app/register/page.js
@@ -0,0 +1,62 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    setCreatingUser(true);
+    await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    setCreatingUser(false);
+    setUserCreated(true);
+  }
+  return (
+    <section className="mt-8">
+      <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      {userCreated && (
+        <div className="my-4 text-center text-green-600">
+          User created.
+          <br /> You can{" "}
+          <Link className="underline" href={"/login"}>
+            Login &raquo;
+          </Link>
+        </div>
+      )}
+      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          disabled={creatingUser}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          disabled={creatingUser}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
+        <div className="my-4 text-center text-gray-500">
+          or login with provider
+        </div>
+        <button className="flex gap-4 justify-center">
+          <Image src={"/google.png"} width={20} height={20} alt="google" />
+          Login with Google
+        </button>
+      </form>
+    </section>
+  );
+}
